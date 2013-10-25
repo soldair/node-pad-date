832082299e731494125584e5ee3a35c512f786e0
alec <alec.hulce@gmail.com>
2013-10-25 16:43:17 -0700

    utc opt

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ module.exports = _formatDate
 module.exports.dbDate = formatDBDate;
 module.exports.fileDate = formatFileDate;
 
-function formatDBDate(date){
+function formatDBDate(date,utc){
   //year-month-day hour:minutes:seconds
-  var o = _formatDate(date);
+  var o = _formatDate(date,utc);
   return o.year+'-'+o.month+'-'+o.day+' '+o.hours+':'+o.minutes+':'+o.seconds;
 }
 
-function formatFileDate(date){
+function formatFileDate(date,utc){
   //year-month-day_hourminutesseconds
-  var o = _formatDate(date);
+  var o = _formatDate(date,utc);
   return o.year+'-'+o.month+'-'+o.day+'_'+o.hours+''+o.minutes+''+o.seconds;
 }
 
-function _formatDate(d){
+function _formatDate(d,utc){
   return {
-    day:pad(d.getDate())
-  , month:pad(d.getMonth()+1)
-  , year:d.getFullYear()
-  , hours:pad(d.getHours())
-  , minutes:pad(d.getMinutes())
-  , seconds:pad(d.getSeconds())
+    day:pad(utc?d.getUTCDate():d.getDate())
+  , month:pad(utc?d.getUTCMonth()+1:d.getMonth()+1)
+  , year:utc?d.getUTCFullYear():d.getFullYear()
+  , hours:pad(utc?d.getUTCHours():d.getHours())
+  , minutes:pad(utc?d.getUTCMinutes():d.getMinutes())
+  , seconds:pad(utc?d.getUTCSeconds():d.getSeconds())
   }
 }
 
